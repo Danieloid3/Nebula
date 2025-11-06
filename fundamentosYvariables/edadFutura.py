3b986dd20c600cb61ba87d7af3ea99d029066883
Maribel Castañeda <maribel@MacBook-Air-de-Daniel.local>
2025-11-06 13:25:36 -0500

    Nivel 1 - Fundamentos y Variables

---
+++ fundamentosYvariables/edadFutura.py
@@ -0,0 +1,5 @@
+print("---Calculadora de edad futura---")
+current_age = int(input("Ingrese su edad actual: "))
+future_age = current_age + 10
+
+print(f"En 10 años, tendrás {future_age} años.")
--- fundamentosYvariables/tipoDeDato.py
+++ fundamentosYvariables/tipoDeDato.py
@@ -1,10 +1,12 @@
 name = "Maribel"
 age = 24
+height = 1.65
 married = True
 array = [1, 2, 3, 4, 5]
 
 print(f"el tipo de dato ingresado es: {type(name)}")   
-print(type(age))    
-print(type(married)) 
-print(type(array))  
+print(f"el tipo de dato ingresado es: {type(age)}") 
+print(f"el tipo de dato ingresado es: {type(height)}")
+print(f"el tipo de dato ingresado es: {type(married)}") 
+print(f"el tipo de dato ingresado es: {type(array)}")  
 
