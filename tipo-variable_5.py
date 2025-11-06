cb4841d78a78adfb9c3849f0342865142411d2dc
salva <totoalahielmejor@gmail.com>
2025-11-06 13:52:55 -0500

    ejercicio 5

---
+++ tipo-variable_5.py
@@ -0,0 +1,17 @@
+nombre=("Salva")
+print(type(nombre))
+
+numero=2
+print(type(numero))
+
+decimal=1.200
+print(type(decimal))
+
+decimal_grande=1.000,0000
+print(type(decimal_grande))
+
+un_caracter='a'
+print(type(un_caracter))
+
+datos_usuario=input
+print(type(datos_usuario))
