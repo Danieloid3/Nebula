cc5b24821dc132cbc9a69e07bb99565c922aa84f
salva608 <totoalahielmejor@gmail.com>
2025-11-07 14:01:13 -0500

    Avanzando en ejercicios

---
+++ nivel 4/buscar_elemento_21.py
@@ -0,0 +1,17 @@
+frutas = ["Manzana", "Pera", "Banano", "Piña"]
+
+nueva_fruta1 = (input("ingresa una fruta: "))
+frutas.append(nueva_fruta1)
+print(frutas)
+
+frutas = ["Manzana", "Pera", "Banano", "Piña"]
+
+nueva_fruta2 = (input("elimina una fruta: "))
+frutas.remove(nueva_fruta2)
+print(frutas)
+
+frutas = ["Manzana", "Pera", "Banano", "Piña"]
+
+nueva_fruta3 = (input("Busca una fruta: "))
+frutas.index(nueva_fruta3)
+print(f"Aqui esta la fruta que necesitas: {nueva_fruta3}")
--- nivel 4/marca.py
+++ nivel 4/marca.py
@@ -4,3 +4,9 @@ nueva_marca = (input("ingresa una marca: "))
 marcas.append(nueva_marca)
 print(marcas)
 
+marcas = ["ford", "mercedes"]
+
+nueva_marca = (input("elimina una marca: "))
+marcas.remove(nueva_marca)
+print(marcas)
+
