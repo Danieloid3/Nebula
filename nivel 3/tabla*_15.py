d442b3bea23bb77726b7b9e96ac8abb94ee40dcf
salva608 <totoalahielmejor@gmail.com>
2025-11-07 09:25:29 -0500

    guardar

---
+++ nivel 3/tabla*_15.py
@@ -0,0 +1,7 @@
+numero_multiplicar=int(input("Ingresa el numero que quieres multiplicar: "))
+
+print(f"Tabla de multiplicar del numero {numero_multiplicar}")
+
+for i in range (1 * 11):
+    resultado=numero_multiplicar*i
+    print(f"El resultado de la tabla de multiplicar del {numero_multiplicar} es= {resultado}")
