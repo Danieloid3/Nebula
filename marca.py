c889a4aac7b58933da3c61b102aafdeabca8fe8f
salva608 <totoalahielmejor@gmail.com>
2025-11-07 12:28:08 -0500

    guardar

--- marca.py
+++
@@ -1,8 +0,0 @@
-marcas = (input("ingresa una marca: "))
-marca = ["ford", "mercedes"]
-marca.append(marcas)
-print(marca)
-
-marcas = (input("ingresa una marca a eliminar: "))
-marca.remove(marcas)
-print(marca)
--- nivel 3/adivina_num_17.py
+++ nivel 3/adivina_num_17.py
@@ -1,3 +1,13 @@
 import random 
 
-numero_secreto= random.randint
+numero_secreto= random.randint(1,5)
+
+adivina=int
+
+while adivina != numero_secreto: 
+    adivina=int(input("Adivina el numero del 1 al 5: "))
+    if  adivina != numero_secreto:
+        print("no es el numero secreto, intentalo de nuevo: ")
+
+    else:
+        print(f"adivinaste el numero: {numero_secreto}")
