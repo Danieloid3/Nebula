6912e9e4a48901d49c4087e8a6db2b28a920fd33
salva608 <totoalahielmejor@gmail.com>
2025-11-07 08:52:43 -0500

    guardar

--- nivel 2/comparador_numeros_12.py
+++ nivel 2/comparador_numeros_12.py
@@ -2,5 +2,8 @@ num1=float(input("ingrese el primer numero: "))
 num2=float(input("ingrese el segundo numero: "))
 num3=float(input("ingrese el tercer numero: "))
 
-mayor=max()
-menor=min()      
+mayor=max(num1,num2,num3)
+menor=min(num1,num2,num3)     
+
+print(f"El numero mayor es: {mayor}")
+print(f"El numero menor es: {menor}")
---
+++ nivel 3/sumatoria_11.py
@@ -0,0 +1,6 @@
+valor=int(input("ingresa el valor de n= "))
+suma=0
+
+for i in range (1, valor + 1):
+    suma += i
+print(f"La sumatoria del 1 al {valor} es: {suma}")
