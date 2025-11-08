7582370dc0c96716fe87b1afc4047fbb7a885e65
salva608 <totoalahielmejor@gmail.com>
2025-11-08 14:10:54 -0500

    Guardar archivos 20 y 21, nivel 4

---
+++ nivel 4/agregar_eliminar_20.py
@@ -0,0 +1,13 @@
+frutas = ["Manzana", "Pera", "Banano", "Piña"]
+
+nueva_fruta = (input("ingresa una fruta: "))
+frutas.append(nueva_fruta)
+print(frutas)
+
+frutas = ["Manzana", "Pera", "Banano", "Piña"]
+
+nueva_fruta = (input("elimina una fruta: "))
+frutas.remove(nueva_fruta)
+print(frutas)
+
+
