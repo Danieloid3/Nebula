bfa84949b36d4dc9b4d2bcef60b31f0f04c550e1
salva608 <totoalahielmejor@gmail.com>
2025-11-08 18:09:08 -0500

    Guardar archivos 22,23,24-nivel 4

---
+++ nivel 4/eliminar_duplicados_24.py
@@ -0,0 +1,5 @@
+lista= [1,2,2,4,3,4,5,3,5]
+
+sin_duplicados=list(set(lista))
+
+print("Aqui esta la lista sin numeros duplicados:", lista)
---
+++ nivel 4/promedio_lista_22.py
@@ -0,0 +1,5 @@
+lista = [4.0, 3.5, 5.0, 2.5, 3.0]
+
+promedio = sum(lista) / len(lista)
+
+print("El promedio general es:", promedio)
