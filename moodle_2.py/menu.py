4b862bd8eeae9ebe5ea3efc7f64d1846b8666b9c
Cohorte 6 <totoalahielmejor@gmail.com>
2025-11-11 07:12:52 -0500

    guardar menu moodle 2 actividad 1

---
+++ moodle_2.py/menu.py
@@ -0,0 +1,36 @@
+print("1.  Agregar producto")
+print("2.  Mostrar inventario")
+print("3.  Calcular estadísticas")
+print("4. Salir")
+
+
+while True:
+    opcion=(input("elige una opcion: "))
+
+
+    if opcion == "1":
+        print("Has entrado a la seccion de Agregar producto")
+
+
+    elif opcion == "2":
+        print("Has entrado a la seccion de Mostrar inventario")    
+
+    elif opcion == "3":
+        print("Has entrado a la seccion de calcular estadistica")
+
+    elif opcion =="4":
+        print("Has salido del menu. Vuelva pronto")
+        break
+     
+    else: 
+        print("Solo hay opciones del 1 al 4, vuelve a ingresar un numero: ")    
+     
+
+  
+     
+
+   
+
+    
+   
+   
