c1cb983d1e03667d9863e7d72743ff5629e3cae1
salva608 <totoalahielmejor@gmail.com>
2025-11-12 13:52:30 -0500

    guardar trabajo en equipo (parte salva)

---
+++ eliminar_pacientes.py
@@ -0,0 +1,19 @@
+eliminar_user=int(input("Ingresa el ID del usuario que quiere eliminar: "))
+
+
+
+def user ():
+    user=[{"ID": eliminar_user}]
+    print("Deseas eliminar el usuario? Copie aceptar o cancelar")
+    if user.remove == "aceptar":
+        print("Haz aceptado, el usuario a sido eliminado")
+        
+    else:
+        print("Has cancelado, el usuario no ha sido eliminado")
+
+id_eliminado= (input("Escribe segun las indicaciones: "))
+
+
+user()
+
+
